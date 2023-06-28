44085768076897f67c5f6699c767225f02dea57f
Anusree-K19 <anusreemanoj19@gmail.com>
2023-06-28 19:07:22 +0530

    Updated login and signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import {Row , Col , From , Input} from 'antd'
+import {Link} from "react-router-dom"
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div classname='Login'>
+           <Row gutter={16} className='d=flex align-items-center'>
+
+               <Col lg={16} style={{position :'relative'}}>
+                   <img src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
+                   <h1 className='login-logo'>PEDALPAL</h1>
+               </Col>
+            
+               <Col lg={8} className='text-left p-5'>
+                   <Form layout='vertical' className='login-form p-5'>
+                       <h1>Login</h1>
+                       <hr />
+
+                       <Form.Item name='username' label='Username' rules={[{required: true}]}>
+                           <Input/>
+
+                       </Form.Item>
+                       <Form.Item name='password' label='Password' rules={[{required: true}]}>
+                           <Input/>
+
+                       </Form.Item>
+
+                       <button className='btn1 mt-2 mb-3'>Login</button>
+                       <hr />
+
+                       <Link to='/register'>Click Here to Register</Link>
+
+                      
+
+                    </Form>
+               </Col>
+                
+
+           </Row>
         </div>
     )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,48 @@
 import React from 'react'
-
+import {Row , Col , From , Input} from 'antd'
+import {Link} from "react-router-dom"
 function Register() {
     return (
-        <div>
-            <h1>Register</h1>
+        <div classname='Register'>
+           <Row gutter={16} className='d=flex align-items-center'>
+
+               <Col lg={16} style={{position :'relative'}}>
+                   <img src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
+                   <h1 className='login-logo'>PEDALPAL</h1>
+               </Col>
+            
+               <Col lg={8} className='text-left p-5'>
+                   <Form layout='vertical' className='login-form p-5'>
+                       <h1>Register</h1>
+                       <hr />
+
+                       <Form.Item name='username' label='Username' rules={[{required: true}]}>
+                           <Input/>
+
+                       </Form.Item>
+                       <Form.Item name='password' label='Password' rules={[{required: true}]}>
+                           <Input/>
+
+                       </Form.Item>
+                       <Form.Item name='cpassword' label='Confirm Password' rules={[{required: true}]}>
+                           <Input/>
+
+                       </Form.Item>
+
+                       <button className='btn1 mt-2 mb-3'>Register</button>
+                       <hr />
+
+                       <Link to='/login'>Click Here to Login</Link>
+
+                      
+
+                    </Form>
+               </Col>
+                
+
+           </Row>
         </div>
     )
 }
 
-export default Register
+export default Login
