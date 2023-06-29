92a9cffb7249d600306b3190754b74112811d716
melvin <melvintk09@gmail.com>
2023-06-29 13:29:33 +0530

    Minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Rental from './pages/Rental'
+import 'antd/dist/reset.css';
 
 function App() {
   return (
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react'
 import DefaultLayout from '../components/DefaultLayout'
 import {useDispatch, useSelector} from 'react-redux'
 import {getAllBicycles} from '../redux/actions/bicycleActions'
-
+import { Button, Row, Col} from 'antd';
+import Spinner from '../components/Spinner';
 function Home() {
     const {bicycles} = useSelector(state => state.bicycleReducer)
+    const {loading} = useSelector(state=>state.alertsReducer)
     const dispatch = useDispatch()
 
+    
     useEffect(() => {
         dispatch(getAllBicycles())
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -14,8 +17,34 @@ function Home() {
 
     return (
         <DefaultLayout>
-            <h1>Home Page</h1>
-            <h1>The length of bicycles array is {bicycles.length}</h1>
+
+                {loading == true && (<Spinner/>)}
+            
+            <Row justify='center' gutter={16} className='mt-5'>
+                {bicycles.map(bicycle=>{
+                    return <Col lg={5} sm={24} xs={24}>
+                        <div className='bicycle p-2 bs1 mt-3'>
+                            <img src={bicycle.image} className='bicycleimg'/>
+                            <div className='bicycle-content d-flex align-items-center justify-content-between'>
+                                <div>
+                                    <p>
+                                        {bicycle.name}
+                                    </p>
+                                    <p>
+                                        {bicycle.rentPerHour}Rent Per Hour/-
+                                    </p>
+                                </div>
+                                <div>
+                                    <button className='btn1 mr-2'>
+                                        Book Now
+                                    </button>
+                                </div>
+                            </div>
+                            </div>
+
+                    </Col>
+                })}
+            </Row>
         </DefaultLayout>
     )
 }
