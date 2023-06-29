5d79eef55b29667ae1695a91320d2b9067dd846d
Rishikesh V K <rishikeshvk2001@gmail.com>
2023-06-29 16:04:37 +0530

    Design changes to Login and Register pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,50 +1,57 @@
 import React from 'react'
 import {Row , Col , Form , Input} from 'antd'
-import {Link} from "react-router-dom"
-import {useDispatch} from 'react-redux'
+import { Link } from 'react-router-dom'
+import {useDispatch , useSelector} from 'react-redux'
 import { userLogin } from '../redux/actions/userActions'
+import AOS from 'aos';
+import Spinner from '../components/Spinner';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 function Login() {
     const dispatch = useDispatch()
-    function onFinish(values){
+    const {loading} = useSelector(state=>state.alertsReducer)
+    function onFinish(values) {
         dispatch(userLogin(values))
         console.log(values)
 
-    }
+ }
     return (
-        <div classname='Login'>
-           <Row gutter={16} className='d=flex align-items-center'>
+        <div className='login'>
+            {loading && (<Spinner />)}
+            <Row gutter={16} className='d-flex align-items-center' >
 
-               <Col lg={16} style={{position :'relative'}}>
-                   <img alt="bicycle" src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
-                   <h1 className='login-logo'>PEDALPAL</h1>
-               </Col>
-            
-               <Col lg={8} className='text-left p-5'>
-                   <Form layout='vertical' className='login-form p-5' onFinish={onFinish}>
-                       <h1>Login</h1>
-                       <hr />
+                <Col lg={16} style={{position: 'relative'}}>
+                    <img 
+                    className='w-100'
+                    data-aos='slide-right'
+                    data-aos-duration='1500'
+                    src="https://askproject.net/venturo/wp-content/uploads/sites/84/2022/06/josh-nuttall-eTrHMJwI5ro-unsplash.jpg"/>
+                     <h1 className='login-logo'>PEDALPAL</h1>
+                </Col>
+                <Col lg={8} className='text-left p-5'>
+                    <Form layout='vertical' className='login-form p-5' onFinish={onFinish}>
+                         <h1>Login</h1>
+                         <hr />
+                         <Form.Item name='username' label='Username' rules={[{required: true}]}>
+                             <Input/>
+                         </Form.Item>
+                         <Form.Item name='password' label='Password' rules={[{required: true}]}>
+                             <Input type='password'/>
+                         </Form.Item>
 
-                       <Form.Item name='username' label='Username' rules={[{required: true}]}>
-                           <Input/>
+                         <button className='btn1 mt-2'>Login</button>
 
-                       </Form.Item>
-                       <Form.Item name='password' label='Password' rules={[{required: true}]}>
-                           <Input/>
+                         <hr />
 
-                       </Form.Item>
-
-                       <button className='btn1 mt-2 mb-3'>Login</button>
-                       <hr />
-
-                       <Link to='/register'>Click Here to Register</Link>
-
-                      
+                         <Link to='/register'>Click Here to Register</Link>
+                       
 
                     </Form>
-               </Col>
-                
+                </Col>
 
-           </Row>
+            </Row>
+
         </div>
     )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,57 +1,69 @@
-import React from 'react'
-import {Row , Col , Form , Input} from 'antd'
-import {Link} from "react-router-dom"
-import {useDispatch} from 'react-redux'
-import { userRegister } from '../redux/actions/userActions'
+import React from "react";
+import { Row, Col, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+import {useDispatch , useSelector} from 'react-redux'
+import { userRegister } from "../redux/actions/userActions";
+import AOS from 'aos';
+import Spinner from '../components/Spinner';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init()
 function Register() {
-    const dispatch = useDispatch()
-
-    function onFinish(values){
-        console.log(values)
-        dispatch(userRegister(values))
-
+  const dispatch = useDispatch()
+  const {loading} = useSelector(state=>state.alertsReducer)
+    function onFinish(values) {
+           dispatch(userRegister(values))
+           console.log(values)
     }
-    return (
-        <div classname='Register'>
-           <Row gutter={16} className='d=flex align-items-center'>
 
-               <Col lg={16} style={{position :'relative'}}>
-                   <img alt="bicycle" src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
-                   <h1 className='login-logo'>PEDALPAL</h1>
-               </Col>
-            
-               <Col lg={8} className='text-left p-5'>
-                   <Form layout='vertical' className="login-form p-5" onFinish={onFinish}>
-                       <h1>Register</h1>
-                       <hr />
+  return (
+    <div className="login">
+      {loading && (<Spinner />)}
+      <Row gutter={16} className="d-flex align-items-center">
+        <Col lg={16} style={{ position: "relative" }}>
+          <img 
+           alt="bicycle"
+           className='w-100'
+           data-aos='slide-left'
+           data-aos-duration='1500'
+          src="https://askproject.net/venturo/wp-content/uploads/sites/84/2022/06/josh-nuttall-eTrHMJwI5ro-unsplash.jpg" />
+          <h1 className="login-logo">PEDALPAL</h1>
+        </Col>
+        <Col lg={8} className="text-left p-5">
+          <Form layout="vertical" className="login-form p-5" onFinish={onFinish}>
+            <h1>Register</h1>
+            <hr />
+            <Form.Item
+              name="username"
+              label="Username"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="cpassword"
+              label="Confirm Password"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
 
-                       <Form.Item name='username' label='Username' rules={[{required: true}]}>
-                           <Input/>
+            <button className="btn1 mt-2 mb-3">Register</button>
+            <br />
 
-                       </Form.Item>
-                       <Form.Item name='password' label='Password' rules={[{required: true}]}>
-                           <Input/>
-
-                       </Form.Item>
-                       <Form.Item name='cpassword' label='Confirm Password' rules={[{required: true}]}>
-                           <Input/>
-
-                       </Form.Item>
-
-                       <button className='btn1 mt-2 mb-3'>Register</button>
-                       <hr />
-
-                       <Link to='/login'>Click Here to Login</Link>
-
-                      
-
-                    </Form>
-               </Col>
-                
-
-           </Row>
-        </div>
-    )
+            <Link to="/login">Click Here to Login</Link>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
